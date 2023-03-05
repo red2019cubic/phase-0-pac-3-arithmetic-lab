bf6a8f61522eaf7ae33d08670e9373566c8a1406
Red <red.sarouf2014@gmail.com>
2023-03-05 07:16:24 -0500

    added Calculator challenge lab

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+    // we'll fill this in shortly
+    return "something";
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract() {
+   return "something";
+}
+
+function subtract(a,b) {
+   return a - b;
+}
+
+function multiply() {
+    return "something";
+}
+
+function multiply(a , b) {
+    return a * b;
+}
+
+function divide() {
+    return "something";
+}
+
+function divide(a , b) {
+    return a / b;
+}
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
